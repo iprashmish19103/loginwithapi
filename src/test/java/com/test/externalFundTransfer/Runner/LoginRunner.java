49e916735dabdf953a03335169472795ff5d9b32
Prashant <Prashant.Mishra@ig.com>
2020-08-02 16:03:24 +0530

    second-commit for reports

--- src/test/java/com/test/externalFundTransfer/Runner/LoginRunner.java
+++ src/test/java/com/test/externalFundTransfer/Runner/LoginRunner.java
@@ -8,9 +8,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/resources/Features/"},
         glue = {"com.test.logincheck.loginwithapi.loginwithapi.StepDefination"},
-        format = {"pretty", "html:test-output", "json:json_output/cucumber.json"},
+        format = {"pretty", "json:target/cucumber-reports/CucumberTestReport.json"},
+        plugin = ("json:target/cucumber-reports/CucumberTestReport.json"),
         monochrome = true,
         strict = true,
         dryRun = false)
+
 public class LoginRunner {
 }
